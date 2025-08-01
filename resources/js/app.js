b9a461283e13f68871a336cb5aae6ed02c00d805
Ikhmal <Ikhmal1410@gmail.com>
2025-08-01 10:00:54 +0800

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ import { createApp, h } from "vue";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 import Toaster from "./components/ui/toast/Toaster.vue";
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = import.meta.env.VITE_APP_NAME || "S-CONNECT";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
