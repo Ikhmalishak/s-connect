178bca2ecaf2bfa787d04d90cc3389f89250f647
Ikhmal <Ikhmal1410@gmail.com>
2026-06-11 17:42:15 +0800

    update

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -9,7 +9,11 @@ class DepartmentSeeder extends Seeder
 {
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
         DB::table('departments')->truncate();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         DB::table('departments')->insert([
             ['name' => 'Engineering', 'created_at' => now(), 'updated_at' => now()],
